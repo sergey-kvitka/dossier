882c1f4f88ec0e6276a334cd3a9c7aa520cbf5ff
sergey-kvitka <45879111+sergey-kvitka@users.noreply.github.com>
2023-01-31 12:05:14 +0400

    initial commit

---
+++ src/main/java/com/kvitka/dossier/DossierApplication.java
@@ -0,0 +1,11 @@
+package com.kvitka.dossier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossierApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DossierApplication.class, args);
+    }
+}
